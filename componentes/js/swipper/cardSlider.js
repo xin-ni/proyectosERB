8bb16369cff033e1c8c6a8ec6e8918fbf49f3f64
Estefany Roman <estefanyromanbroncano@gmail.com>
2023-09-06 17:52:33 -0500

    card js, html, css completos con interaccion y estilos -falta comentar codigo-

--- componentes/js/swipper/cardSlider.js
+++ componentes/js/swipper/cardSlider.js
@@ -1,7 +1,83 @@
+const wrapper = document.querySelector(".contenedorSlide");
 const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".cardContent").offsetWidth;
+const arrowBtns = document.querySelectorAll(".contenedorSlide i");
+const carouselChildrens = [...carousel.children];
 
-const dragging =(e)=>{
-    carousel.scrollLeft=e.pageX;
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+// Calcular la cantidad de tarjetas que deben mostrarse inicialmente
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// Clonar las tarjetas al principio y al final del carrusel para el desplazamiento infinito
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Función para ajustar la cantidad de tarjetas visibles
+const updateCardPerView = () => {
+    cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 }
 
-carousel.addEventListener("mousemove", dragging)
+// Desactivar las transiciones temporales para evitar problemas visuales
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        updateCardPerView();
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if (!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const infiniteScroll = () => {
+    if (carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+
+    clearTimeout(timeoutId);
+    if (!wrapper.matches(":hover")) autoPlay();
+}
+
+const autoPlay = () => {
+    if (window.innerWidth < 400 || !isAutoPlay) return;
+    updateCardPerView();
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2000);
+}
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
